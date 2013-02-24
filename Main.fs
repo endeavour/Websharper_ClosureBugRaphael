d9972e74a7dd8c9225fc3a491ed2a48fc92338fc
James Freiwirth <james@perfectshuffle.co.uk>
2013-02-24 20:27:59 +0000

    Closure bug sample

---
+++ Main.fs
@@ -0,0 +1,71 @@
+﻿namespace HtmlSite1
+
+open System
+open System.IO
+open System.Web
+open IntelliFactory.WebSharper.Sitelets
+
+/// Defines a sample HTML site with nested pages
+module SampleSite =
+    open IntelliFactory.WebSharper
+    open IntelliFactory.Html
+
+    // Action type
+    type Action =
+        | Index
+
+    module Skin =
+
+        type Page =
+            {
+                Title : string
+                Body : list<Content.HtmlElement>
+            }
+
+        let MainTemplate =
+            let path = Path.Combine(__SOURCE_DIRECTORY__, "Main.html")
+            Content.Template<Page>(path)
+                .With("title", fun x -> x.Title)
+                .With("body", fun x -> x.Body)
+
+        let WithTemplate title body : Content<Action> =
+            Content.WithTemplate MainTemplate <| fun context ->
+                {
+                    Title = title
+                    Body = body context
+                }
+
+    // Module containing client-side controls
+    module Client =
+        open IntelliFactory.WebSharper.Html
+
+        type MyControl() =
+            inherit IntelliFactory.WebSharper.Web.Control ()
+
+            [<JavaScript>]
+            override this.Body =
+                I [Text "Client control"] :> IPagelet
+
+    let Index =
+        Skin.WithTemplate "Index page" <| fun ctx ->
+            [
+                P [Text "Clicking on a square should change its colour"]
+                Div [new SquareControl()]
+            ]
+
+    let MySitelet = Sitelet.Content "/index" Action.Index Index
+
+    // Actions to generate pages from
+    let MyActions =
+        [
+            Action.Index
+        ]
+
+/// The class that contains the website
+type MySampleWebsite() =
+    interface IWebsite<SampleSite.Action> with
+        member this.Sitelet = SampleSite.MySitelet
+        member this.Actions = SampleSite.MyActions
+
+[<assembly: WebsiteAttribute(typeof<MySampleWebsite>)>]
+do ()
---
+++ Squares.fs
@@ -0,0 +1,36 @@
+﻿[<AutoOpen>]
+module Squares
+open IntelliFactory.WebSharper.Formlet
+open IntelliFactory.WebSharper
+open IntelliFactory.WebSharper.Html
+open IntelliFactory.WebSharper.JQuery
+open IntelliFactory.WebSharper.Raphael
+
+type SquareControl() =
+  inherit Web.Control()
+
+  [<JavaScript>]
+  let gen() =
+    let id = "paper"
+
+    Div[Attr.Id id] |>! OnAfterRender (fun el ->
+      let paper = Raphael(id, 300., 300.)
+      
+      let path1 = Raphael.Path().Absolute.MoveTo(0.,0.).LineTo(0.,100.).LineTo(100.,100.).LineTo(100.,0.).LineTo(0.,0.)
+      let path2 = Raphael.Path().Absolute.MoveTo(100.,100.).LineTo(100.,200.).LineTo(200.,200.).LineTo(200.,100.).LineTo(100.,100.)
+      
+      let path1' = paper.Path(path1).Fill("#CCC")
+      let path2' = paper.Path(path2).Fill("#CCC")    
+      
+      // Fails
+      for p in [path1';path2'] do
+        p.Click(fun() -> p.Fill("#000") |> ignore) |> ignore
+    )
+
+    // Works
+    //path1'.Click(fun() -> path1'.Fill("#000") |> ignore) |> ignore
+    //path2'.Click(fun() -> path2'.Fill("#000") |> ignore) |> ignore
+
+  [<JavaScript>]
+  override this.Body =
+    upcast gen()
